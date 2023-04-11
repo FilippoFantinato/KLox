ab88f776102d6996c63db9831829e233580913d8
FilippoFantinato <fantinato.filippo99@gmail.com>
2023-04-11 09:18:17 +0200

    Base project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun runREPL() {
+
+}
+
+fun runFile(file: String){
+
+}
+
+fun main(args: Array<String>) {
+    args.forEach { file -> print(file) }
+//    if(args.isEmpty())
+//    {
+//        runREPL()
+//    }
+//    else
+//    {
+//        args.forEach { file -> runFile(file) }
+//    }
+}
